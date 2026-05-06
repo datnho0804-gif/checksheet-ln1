764995a0eecaabc074b20363f5617728493db84c
datnho0804-gif <datnho0804@gmail.com>
2026-05-06 11:23:15 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'checksheet-ln1-cache-v1';
+const assets = [
+  './IP.html',
+  './manifest.json',
+  'https://unpkg.com/exceljs/dist/exceljs.min.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+];
+
+// Cài đặt và lưu các file cần thiết vào bộ nhớ đệm
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Trả về dữ liệu từ bộ nhớ đệm khi mất mạng
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
